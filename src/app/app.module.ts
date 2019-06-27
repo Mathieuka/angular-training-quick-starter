1f63753d9f67c526870de09ec775328285e81921
KAHLAOUI Mathieu <mathieu.kahlaoui@oscaro.com>
2019-06-27 12:00:21 +0200

    heroes component and Hero class created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';  // IMPORTANT: The most important @NgModule decorator annotates the top-level AppModule class.
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ // all ngModule "import" need to be put here in "imports"
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
